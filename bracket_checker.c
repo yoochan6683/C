2a37c7289c4543966fe586daaf46332af406d564
최유찬 <c01051213095@gmail.com>
2024-02-02 18:04:06 +0900

    bracket_checker returns 1 if valid, 0 if not valid

--- bracket_checker.c
+++ bracket_checker.c
@@ -19,33 +19,67 @@ int is_full(bracket_stack *bs) {
 }
 
 int is_empty(bracket_stack *bs) {
-    return bs -> top == -1;
+    return (bs -> top) == -1;
 }
 
 void push(bracket_stack *bs, char bracket) {
     if(is_full(bs)) {
         bs -> capacity *= 2;
-        bs -> brackets = (char *)realloc(bs, sizeof(char)*bs -> capacity);
+        bs -> brackets = (char *)realloc(bs -> brackets, sizeof(char)*bs -> capacity);
     }
-    bs -> brackets[++bs -> top] = bracket;
+    bs -> brackets[++(bs -> top)] = bracket;
 }
 
 char pop(bracket_stack *bs) {
-    if(is_empty) {
-        printf("UnderFlow");
-        exit(1);
+    if(is_empty(bs)) {
+        return 'e';
     }
     else return bs -> brackets[bs -> top--];
+}
+
+int check_braket(char * code, bracket_stack *bs) {
+    char latest_bracket;
+    while(*code != '\0') {
+        switch (*code)
+        {
+        case '(': case '{': case '[':
+            push(bs, *code);
+            break;
+        case ')':
+            latest_bracket = pop(bs);
+            if(latest_bracket != '(' || latest_bracket == 'e') return 0;
+            break;
+        case '}':
+            latest_bracket = pop(bs);
+            if(latest_bracket != '{' || latest_bracket == 'e') return 0;
+            break;
+        case ']':
+            latest_bracket = pop(bs);
+            if(latest_bracket != '[' || latest_bracket == 'e') return 0;
+            break;
+        }
+        code++;
+    }
+    if(!is_empty(bs)) return 0;
+    return 1;
 }
 
 
 
 int main() {
+    //스택 초기 설정
     bracket_stack s;
     init_bracket_stack(&s);
 
+    //입력된 코드 저장
+    char code[100];
+    char* p_code = code;
+    scanf("%s", code);
+
+    //괄호 검사 함수
+    int is_valid = check_braket(code, &s);
+    printf("%d", is_valid);
 
 
-    printf("hello world!");
     return 0;
 }
